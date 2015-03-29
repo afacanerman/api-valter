a93a7ef2ebe9703ea2ab2ee862d0e14a51fe5d83
Cagdas Erman AFACAN <cagdas.afacan@hepsiburada.com>
2015-03-30 00:24:46 +0300

    type check added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var program = require('commander');
-var search = require("./lib/search.js");
+var valter = require("./lib/valter.js");
 
 
 program
@@ -9,14 +9,37 @@ program
   .option('-a, --against [value]', 'Runs contract validation against', 'http://localhost')
   .parse(process.argv);
 
-if (program.against === undefined) { throw new Error("against argument is mandatory for help -h"); } 
-if (program.filepath === undefined) { throw new Error("filepath argument is mandatory for help -h");  }
+if (program.against === undefined) { throw "against argument is mandatory for help -h"; } 
+if (program.filepath === undefined) { throw "filepath argument is mandatory for help -h";  }
 
 console.log('valter started against \'%s\' for the metada files under \'%s\'\n\n',
              program.against, program.filepath);
 
 // Program is ready to run contract tests
-var contractList = search.scan(program.filepath);
+var contractList = valter.scan(program.filepath);
+var contract = {
+	e: "data",
+	r: 123,
+	m: [1, 2, 3],
+	a: {
+	  b:1
+	},
+	n: new Date()
+};
 
-console.log(contractList);
+
+var serviceResponse = {
+	e: "data",
+	r: 123,
+	m: [1, 2, 3],
+	a: {
+	  b:1
+	},
+	n: new Date(),
+	t: ""
+};
+
+valter.typeCheck(contract, serviceResponse);
+
+console.log("SUCCESS");
 
